61aaf76c7161dc91a1ee672aae267c2e2ab2d27e
David Tyler <david.tyler@metaswitch.com>
2021-05-10 12:32:19 +0000

    Added support for retrieving ranges of values in base64 encoded format (particularly for retrieving VRAM)

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,14 @@ fn read_byte(id: String, address: u16, in_memory_state: State<InMemoryState>) ->
     format!("{}", reader.get(&id).unwrap()[address as usize])
 }
 
+#[get("/readRange?<id>&<address>&<length>")]
+fn read_range(id: String, address: u16, length: u16, in_memory_state: State<InMemoryState>) -> String {
+    let reader = in_memory_state.state.read().unwrap();
+    let data = reader.get(&id).unwrap();
+    let address_range = (address as usize)..(address as usize + length as usize);
+    format!("{}", base64::encode(&data[address_range]))
+}
+
 #[get("/status")]
 fn status() -> &'static str {
     "Healthy"
@@ -52,7 +60,7 @@ fn rocket() -> rocket::Rocket {
     };
 
     rocket::ignite()
-        .mount("/api/v1", routes![read_byte, write_byte, initialise])
+        .mount("/api/v1", routes![read_byte, read_range, write_byte, initialise])
         .mount("/", routes![status])
         .manage(in_memory_state)
 }
@@ -75,6 +83,36 @@ mod test {
         let mut response = client.get("/status").dispatch();
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.body_string(), Some("Healthy".into()));
+    }
+
+    #[test]
+    fn test_read_range() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let program_state = Box::new(Rom { id: "abcd".to_string(), program_state: base64::encode(vec![0x0; 0x10000]) });
+        let rom = serde_json::to_string(program_state.as_ref()).unwrap();
+        let initialise_response = client
+            .post("/api/v1/initialise")
+            .header(ContentType::JSON)
+            .body(rom)
+            .dispatch();
+        assert_eq!(initialise_response.status(), Status::Ok);
+
+        let write_response1 = client
+            .post(format!("/api/v1/writeByte?id={}&address={}&value={}", "abcd", 0x10, 0xFF))
+            .body("")
+            .dispatch();
+        assert_eq!(write_response1.status(), Status::Ok);
+        let write_response2 = client
+            .post(format!("/api/v1/writeByte?id={}&address={}&value={}", "abcd", 0x1A, 0xFE))
+            .body("")
+            .dispatch();
+        assert_eq!(write_response2.status(), Status::Ok);
+
+        let mut read_range_response = client
+            .get(format!("/api/v1/readRange?id={}&address={}&length={}", "abcd", 0x10, 0x10))
+            .dispatch();
+        assert_eq!(read_range_response.status(), Status::Ok);
+        assert_eq!(read_range_response.body_string(), Some(base64::encode(vec![0xFF, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xFE, 0x0, 0x0, 0x0, 0x0, 0x0])));
     }
 
     #[test]
